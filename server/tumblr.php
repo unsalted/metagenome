eeb4801e24701a9897469d188aa94bce09a1cad2
unsalted <nick@unsalted.nu>
2014-11-13 00:58:27 -0800

    put photo

--- server/tumblr.php
+++ server/tumblr.php
@@ -1,7 +1,31 @@
 <?php
 
-$encoded = $_POST['image'];
-$decoded = base64_decode($encoded);
+// make sure the image-data exists and is not empty
+// xampp is particularly sensitive to empty image-data 
+if ( isset($_POST["image"]) && !empty($_POST["image"]) ) {    
+
+    // get the dataURL
+    $dataURL = $_POST["image"];  
+
+    // the dataURL has a prefix (mimetype+datatype) 
+    // that we don't want, so strip that prefix off
+    $parts = explode(',', $dataURL);  
+    $data = $parts[1];  
+
+    // Decode base64 data, resulting in an image
+    $data = base64_decode($data);  
+
+    // create a temporary unique file name
+    $file = './uploads/' . uniqid() . '.png';
+
+    // write the file to the upload directory
+    $success = file_put_contents($file, $data);
+
+    // return the temp file name (success)
+    // or return an error message just to frustrate the user (kidding!)
+    print $success ? $file : 'Unable to save this image.';
+
+}
  
 #Requires PHP 5.3.0
  
@@ -56,7 +80,7 @@ function oauth_sig($method, $uri, $params) {
  
  
 $headers = array("Host" => "http://api.tumblr.com/", "Content-type" => "application/x-www-form-urlencoded", "Expect" => "");
-$params = array("data" => $decoded, "type" => "photo");
+$params = array("data" => array(file_get_contents($file), "type" => "photo");
  
 $blogname = "metagenome.tumblr.com";
 oauth_gen("POST", "http://api.tumblr.com/v2/blog/$blogname/post", $params, $headers);
